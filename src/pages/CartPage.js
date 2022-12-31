1323cb2e3cf0ebffe393a871d945316c905c435f
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-31 11:59:52 +0530

    s

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { phoneAfterLogin } from './loginpg'
 export default function CartPage() {
   return (
     <>
       <div>CartPage</div>
-      <div>{phoneAfterLogin}</div>
+      <div></div>
     </>
 
   )
