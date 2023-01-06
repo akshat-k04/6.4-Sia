25c729980eb40f03b28291e9a7adae67b5aae852
Akshat <a_khandelwal@ch.iitr.ac.in>
2023-01-06 13:11:00 +0530

    sm

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <>
           <div className="card w-100" >
         <img src="" className="card-img-top"  width={10} alt="..."/>
                   <div className="card-body">
-                      <h5 className="card-title">Card title</h5>
-                      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="/" className="btn btn-primary">Go somewhere</a>
+                      <h5 className="card-title">{props.name}</h5>
+                      <p className="card-text">{props.disc}</p>
+                      <a href="/" className="btn btn-primary">{props.price}</a>
                   </div>
           </div>
     </>
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Card from '../components/Card'
 import Carousel from '../components/Carousel'
 import './../css/homescreen.css'
 export default function HomeScreen() {
-
+    const [article, settate]= useState({});
+    
+    useEffect(()=>{async function fc () {
+        let data = await fetch("http://localhost:3000/owner/find", { method: "POST", data: "" });
+        let parsedata = await data.json();
+        settate(parsedata);
+    } fc();},[]);
+    
   return (
     <>
     
     <Carousel/>
 
-    <div className="row">
-        <div className="col-md-3 my-3">
-            <Card />
-        </div>  
-              <div className="col-md-3 my-3">
-                  <Card />
-              </div><div className="col-md-3 my-3">
-                  <Card />
-              </div><div className="col-md-3 my-3">
-                  <Card />
-              </div><div className="col-md-3 my-3">
-                  <Card />
-              </div> 
+          
+              <div className='row'>
+              {Object.keys(article).map((element) => {
+                  return <div className='col-md-4 my-3' key={article[element].id}>
+                      {/* {console.log(article[element])} */}
+                      <Card name={article[element].name} id={article[element].id} disc={article[element].discription} image={article[element].image} type={article[element].type} price={article[element].price} quantity={article[element].quantity} />
+                  </div>
+              })}
+              
+              
+
+
+        
     </div>
           <center>
               <div className="homebox">
