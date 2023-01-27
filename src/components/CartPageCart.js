d35a37d54cfd8358c11df1d8da2534c5082c53c9
SidxRengen <gautamsiddharth226@gmail.com>
2023-01-27 23:06:39 +0530

    some changes in cart

--- src/components/CartPageCart.js
+++ src/components/CartPageCart.js
@@ -1,44 +1,194 @@
-import React from 'react'
-import './../css/CartCard.css'
+import React from "react";
+import "./../css/CartCard.css";
 export default function CartPageCart() {
-    return (
-        <>
+  return (
+    <>
+      <div className="row">
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
+                </div>
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
 
-            <div class="outer">
-                <div class="col">
-                    1 of 3
+                  <span className="material-symbols-outlined dlt">delete</span>
                 </div>
-                <div class="col-6">
-                    2 of 3 (wider)
-                </div>
-                <div class="col">
-                    3 of 3
-                </div>
+              </div>
             </div>
-
-            <div className="outer">
-                <div className="left">
-                    <img className='img' src='/assets/siaLogo.jpg' ></img>
-                    <div className="info">
-                        <h6 className="nam">product name,{"(product id)"}</h6>
-                        <p className="pric">price</p>
-                    </div>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
                 </div>
-                <div className="right">
-                    <div className="operation">
-                        
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
 
-                            <center className="numbr">
-                                quant
-                                <span className="material-symbols-outlined rm">remove</span>
-                                <span className="material-symbols-outlined add">add</span>
-                            </center>
-                            
-                            <span className="material-symbols-outlined dlt" >delete</span>
-                    
-                    </div>
+                  <span className="material-symbols-outlined dlt">delete</span>
                 </div>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
+                </div>
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
+
+                  <span className="material-symbols-outlined dlt">delete</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
+                </div>
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
+
+                  <span className="material-symbols-outlined dlt">delete</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
+                </div>
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
+
+                  <span className="material-symbols-outlined dlt">delete</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-6 col-md-12">
+          <div className="container-fluid1">
+            <div className="row">
+              <div className="col-3 col1">
+                <img className="img" src="/assets/siaLogo.jpg"></img>
+              </div>
+              <div className="col-5 col1">
+                <div className="info">
+                  <h6 className="nam">product name,{"(product id)"}</h6>
+                  <p className="pric">price</p>
+                </div>
+              </div>
+              <div className="col-4 col1">
+                <div className="operation">
+                  <center className="numbr">
+                    quant
+                    <span className="material-symbols-outlined rm">remove</span>
+                    <span className="material-symbols-outlined add">add</span>
+                  </center>
+
+                  <span className="material-symbols-outlined dlt">delete</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="outer">
+        <div className="col-4">
+          <div className="left">
+            <img className="img" src="/assets/siaLogo.jpg"></img>
+          </div>
+          <div className="col-4">
+            <div className="info">
+              <h6 className="nam">product name,{"(product id)"}</h6>
+              <p className="pric">price</p>
+            </div>
+          </div>
+          <div className="col-4">
+            <div className="operation">
+              <center className="numbr">
+                quant
+                <span className="material-symbols-outlined rm">remove</span>
+                <span className="material-symbols-outlined add">add</span>
+              </center>
+
+              <span className="material-symbols-outlined dlt">delete</span>
+            </div>
+          </div>
+        </div>
+      </div> */}
+    </>
+  );
 }
